1089dbaa73593c3709f94908c51349596ca012fe
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-12 01:09:27 +0300

    update tcp server

--- GolangWebDevelopment/02Server/015_understanding-TCP-servers/01_write/main.go
+++ GolangWebDevelopment/02Server/015_understanding-TCP-servers/01_write/main.go
@@ -13,14 +13,16 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer li.Close()
-
+	//Infinete loop for wating clients
 	for {
 		conn, err := li.Accept()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
+		fmt.Println("Connection Established")
 
+		// write to connection
 		io.WriteString(conn, "\nHello from TCP server\n")
 		fmt.Fprintln(conn, "How is your day?")
 		fmt.Fprintf(conn, "%v", "Well, I hope!")
