aae4adfa2b4c6303b7e85c8d2786705657e64ab2
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-11 20:05:25 +0300

    add text template with template.ParseFiles

--- GolangWebDevelopment/1Templates/03/01_stdout/main.go
+++ GolangWebDevelopment/1Templates/03/01_stdout/main.go
@@ -6,16 +6,20 @@ import (
 	"text/template"
 )
 
-func main() {
-	tpl, err := template.ParseFiles("tpl.gohtml")
+func checkError(err *error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
 
+//https://pkg.go.dev/text/template
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml") //templete is struct impelemented parse tree ==> https://youtu.be/a1OLDU1QAfw
+	checkError(&err)
 	err = tpl.Execute(os.Stdout, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	checkError(&err)
+	//fmt.Println(tpl.Root)
+	//fmt.Println(tpl.Tree)
 }
 
 // Do not use the above code in production
