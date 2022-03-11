aed7e0f1c8798559a6fe907e5217693025822718
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-11 19:43:45 +0300

    create template with file write ops

--- GolangWebDevelopment/1Templates/02/02_file/main.go
+++ GolangWebDevelopment/1Templates/02/02_file/main.go
@@ -8,9 +8,34 @@ import (
 	"strings"
 )
 
+func TemplateWithFile(s string) {
+	tpl := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Hello World!</title>
+</head>
+<body>
+<h1>` +
+		s +
+		`</h1>
+</body>
+</html>
+	`
+	f, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("error creating file", err)
+	}
+	defer f.Close()
+
+	io.Copy(f, strings.NewReader(tpl))
+}
+
 func main() {
-	name := "Todd McLeod"
-	str := fmt.Sprint(`
+	name := "Erdem Ozgen"
+	//you can use sprint here
+	str := fmt.Sprint(` 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,12 +49,7 @@ func main() {
 </body>
 </html>
 	`)
+	fmt.Println(str)
+	TemplateWithFile(name)
 
-	nf, err := os.Create("index.html")
-	if err != nil {
-		log.Fatal("error creating file", err)
-	}
-	defer nf.Close()
-
-	io.Copy(nf, strings.NewReader(str))
 }
