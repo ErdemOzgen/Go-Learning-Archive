7f53201aa6f3513e4f8bca9eb2f3f09ae470f849
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-11 21:10:41 +0300

    add parse template with data

--- GolangWebDevelopment/1Templates/03/05_performant-parsing_func-init/main.go
+++ GolangWebDevelopment/1Templates/03/05_performant-parsing_func-init/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"text/template"
@@ -8,16 +9,25 @@ import (
 
 var tpl *template.Template
 
-func init() {
+func init() { // use parser before main
 	tpl = template.Must(template.ParseGlob("templates/*"))
+
+	/*
+		init is called after all the variable declarations
+		in the package have evaluated their initializers,
+		and those are evaluated only after all the imported
+		 packages have been initialized.
+	*/
 }
 
 func main() {
+
+	fmt.Println("-------------")
 	err := tpl.Execute(os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
+	fmt.Println("\n-------------")
 	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
