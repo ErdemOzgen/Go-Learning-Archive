b3a32a91696509152a2254199047aaf2fb4c5b89
ErdemOzgen <m.erdemozgen@gmail.com>
2022-03-20 21:55:11 +0300

    add extre test case

--- GolangTesting/Part02/integers/v1/adder_test.go
+++ GolangTesting/Part02/integers/v1/adder_test.go
@@ -10,3 +10,14 @@ func TestAdder(t *testing.T) {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
 }
+
+func TestAdder2(t *testing.T) {
+	t.Run("testing for 2+3=5", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 5
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+}
